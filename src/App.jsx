26664dcd96e209265b1187060bda54e45b1c8780
abdullahbaa <aaziz191231@bscse.uiu.ac.bd>
2026-04-22 20:28:06 +0600

    Install tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import LoginRegister from './assets/Components/Login&Register/LoginRegister'
+// import LoginRegister from './assets/Components/Login&Register/LoginRegister'
+import Homepage from '../src/assets/Components/Allpages/Homepage'
 function App() {
   
 
   return (
     <>
       <div>
-        <LoginRegister/>
+        {/* <LoginRegister/> */}
+        <Homepage/>
       </div>
     </>
   )
